12f0abefc76182266740ade84d637a5d84910d6f
Пользователь <meowd098@gmail.com>
2025-02-18 19:06:59 +0300

    Добавьте файлы проекта.

---
+++ Calculator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionCalculator
+{
+    //Calculator:
+    //Stack<int> – стек операндов.
+    //Методы обработки операций.
+    //Методы:
+    //Evaluate(string expression): Вычислить выражение.
+    internal class Calculator
+    {
+        public Stack<int> _operand = new Stack<int>();
+
+        public int Evaluate(string expression)
+        {
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (Int32.TryParse($"{expression[i]}", out int number)) // Странный костыль с $"{expression[i]}"
+                {
+                    _operand.Push(number);
+                }
+
+                else if (($"{expression[i]}") == " ")
+                {
+                    continue;
+                }
+
+                else
+                {
+                    int operand2 = _operand.Pop();
+                    int operand1 = _operand.Pop();
+
+                    string expressionOperator = $"{expression[i]}";
+
+                    switch (expressionOperator)
+                    {
+                        case "+":
+                            _operand.Push(operand1 + operand2);
+                            break;
+                        case "-":
+                            _operand.Push(operand1 - operand2);
+                            break;
+                        case "*":
+                            _operand.Push(operand1 * operand2);
+                            break;
+                        case "/":
+                            _operand.Push(operand1 / operand2);
+                            break;
+                        default:
+                            return -1;
+                    }
+                }
+
+            }
+
+            return _operand.Peek();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ExpressionCalculator
+{
+    internal class Program
+    {
+        //        4. Калькулятор выражений(Stack)
+        //Задача:
+        //Реализовать калькулятор, который обрабатывает выражения в обратной польской записи.
+
+        //Пример работы калькулятора:
+        //"3 4 + 2 *" → 14.
+
+        //Входные данные примерные, напиши свои с клавиатуры:
+        //var calc = new Calculator();
+        //        Console.WriteLine(calc.Evaluate("3 4 + 2 *"));
+        //Ожидаемый результат:
+        //Вывод: 14.
+        static void Main()
+        {
+            var calc = new Calculator();
+            Console.WriteLine(calc.Evaluate("3 4 + 2 *"));
+        }
+    }
+}
